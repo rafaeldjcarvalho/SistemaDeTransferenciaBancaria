917fc4750d7ac78a7323d4821f9f9a4954a0d269
Rafael de Jesus Carvalho <141766102+rafaeldjcarvalho@users.noreply.github.com>
2025-05-26 14:43:36 -0300

    feat: User entity update

--- src/main/java/com/rafael/SistemaDeTransferenciaBancaria/domain/user/User.java
+++ src/main/java/com/rafael/SistemaDeTransferenciaBancaria/domain/user/User.java
@@ -3,25 +3,30 @@ package com.rafael.SistemaDeTransferenciaBancaria.domain.user;
 
 import java.math.BigDecimal;
 
+import com.rafael.SistemaDeTransferenciaBancaria.domain.dtos.UserDTO;
 import com.rafael.SistemaDeTransferenciaBancaria.domain.enums.UserType;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity
+@Entity(name="users")
 @Table(name="users")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class User {
 	
 	@Id
@@ -42,6 +47,17 @@ public class User {
 	
 	private BigDecimal saldo;
 	
+	@Enumerated(EnumType.STRING)
 	private UserType tipo;
+	
+	public User(UserDTO dados) {
+		setNome(dados.nome());
+		setSobrenome(dados.sobrenome());
+		setDocumento(dados.documento());
+		setEmail(dados.email());
+		setSenha(dados.senha());
+		setSaldo(dados.saldo());
+		setTipo(dados.tipo());
+	}
 
 }
